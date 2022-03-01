6e830f8a272ab14386487a219f12108dbdf238cf
Larina <Losiknor@gmail.com>
2022-03-01 17:28:02 +0700

    Решение задачи 6 обход каталога

---
+++ obhod_directorii/Program.cs
@@ -0,0 +1,28 @@
+﻿// Посмотреть и запомнить информацию о файлах, хранящихся в директории.
+// string path = "C:/_moi/GB/Знакомство с языками программирования/Лекции/Lec_7/perebor_bukv";
+// DirectoryInfo di = new DirectoryInfo(path); System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+// Обход каталога с рекурсией
+void CatalogInfo(string path, string ident = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{ident}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, ident + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{ident}{files[i].Name}");
+    }
+}
+string path = @"C:/_moi/Б А Р Д Ы";
+CatalogInfo(path);
