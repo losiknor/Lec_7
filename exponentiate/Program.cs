f88f939b952f54b6e9539c0c9a76c01ff69af728
Larina <Losiknor@gmail.com>
2022-03-01 15:36:02 +0700

    Решение задачи 4 про возведение в степень с укороченной записью тела метода

---
+++ exponentiate/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вычислить a в степени n
+// 1 способ без рекурсии
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+
+// 2 способ с рекурсией
+int PowerRec(int a, int n)
+{
+    if (n== 0) return 1;
+    else return PowerRec(a, n-1) * a;
+}
+
+// 3 способ - как 2, но короче записано
+int PowerRec1(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec1(a, n-1) * a; 
+}
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine(PowerRec(2, 10));
+Console.WriteLine(PowerRec1(2, 10));
