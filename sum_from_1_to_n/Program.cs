9757229eb40af3f61cefbf0771bc5891b03ed225
Larina <Losiknor@gmail.com>
2022-03-01 14:27:05 +0700

    Решение двух первых задач 1 и 2

---
+++ sum_from_1_to_n/Program.cs
@@ -0,0 +1,17 @@
+﻿// Сумма чисел от 1 до n
+// 1 способ без рекурсии
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+// 2 способ с рекурсией
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n-1);
+}
+Console.WriteLine(SumFor(10));
+Console.WriteLine(SumRec(10));
